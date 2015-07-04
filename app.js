ca291e38060fe2fe6a4c7c8c294d9fe6ee535432
Jorge Velez <jlvelez2@gmail.com>
2015-07-04 16:35:07 -0600

    data tag changed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var io = require('socket.io').listen(3000);
+var io = require('socket.io').listen(8090);
  
 io.sockets.on('connection', function (socket) {
  
 socket.on('message', function (message) {
 console.log("Got message: " + message);
-io.sockets.emit('pageview', { 'url': message });
+io.sockets.emit('pageview', { 'data': message });
 });
  
 }); 
